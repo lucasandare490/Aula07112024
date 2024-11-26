62b25bbe538cc2307c7da743295127cdd707b18e
Lucas Andre cardoso de Lima <lucas2004@gmail.com>
2024-11-26 20:23:17 -0300

    aula10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React from 'react';
+import Menu from './components/MenuComponent';
 
 function App() {
-  return (
-    <div className="App">
- <Navbar dark color="primary">
- <div className="container">
- <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
- <div>Aluno: Lucas Andre</div>
- </div>
- </Navbar>
-
-
-    </div>
-  );
+    return (
+        <div>
+            <Menu />
+        </div>
+    );
 }
 
 export default App;
-
-/*import logo from './logo.svg';*/
